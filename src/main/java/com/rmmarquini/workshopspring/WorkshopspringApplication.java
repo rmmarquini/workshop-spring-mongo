4a931af92a967a946a40c955c5bc040234ead38b
Rafa Marquini <rafael.mardegan@routeasy.com.br>
2021-11-08 13:57:03 -0300

    Initial commit

---
+++ src/main/java/com/rmmarquini/workshopspring/WorkshopspringApplication.java
@@ -0,0 +1,13 @@
+package com.rmmarquini.workshopspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rmmarquini/workshopspring/WorkshopspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmmarquini.workshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
