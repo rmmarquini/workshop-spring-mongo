38e89b495b3d3010833bd69633df31d05699813d
Rafa Marquini <rafael.mardegan@routeasy.com.br>
2021-11-09 11:50:47 -0300

    Finish user crud

--- src/main/java/com/rmmarquini/workshopspring/resources/UserResource.java
+++ src/main/java/com/rmmarquini/workshopspring/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.rmmarquini.workshopspring.dto.UserDTO;
 import com.rmmarquini.workshopspring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,6 +35,31 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+        User user = service.fromDTO(userDTO);
+        service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+        User user = service.fromDTO(userDTO);
+        user.setId(id);
+        service.update(user);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/rmmarquini/workshopspring/services/UserService.java
+++ src/main/java/com/rmmarquini/workshopspring/services/UserService.java
@@ -1,6 +1,7 @@
 package com.rmmarquini.workshopspring.services;
 
 import com.rmmarquini.workshopspring.domain.User;
+import com.rmmarquini.workshopspring.dto.UserDTO;
 import com.rmmarquini.workshopspring.repository.UserRepository;
 import com.rmmarquini.workshopspring.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,34 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("User not founded."));
+    }
+
+    public User insert(User user) {
+        return repository.insert(user);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        repository.deleteById(id);
+    }
+
+    public User update(User user) {
+        User newUser = findById(user.getId());
+        updateData(newUser, user);
+        return repository.save(newUser);
+    }
+
+    private void updateData(User newUser, User user) {
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(
+                userDTO.getId(),
+                userDTO.getName(),
+                userDTO.getEmail()
+        );
     }
 
 }
